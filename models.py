ba07473cb5fc86c6a6313c8fb9691e170ff3272d
Algus <64106681+alvaro865@users.noreply.github.com>
2020-06-25 07:37:44 -0500

    Update models.py
    definiendo atributos

--- models.py
+++ models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Destination:
-	id : int
-	name : str
-	img :str
-	desc :str
-	price :int
-	offer : bool
+	idCiudad : int
+	nombreCiudad : str
+	imagenCiudad :str
+	descripcionCiudad :str
+	precioTour :int
+	ofertaTour : bool
