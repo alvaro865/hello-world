61d035f2bb11b01e3cce5b46b5c840e37623943f
Algus <64106681+alvaro865@users.noreply.github.com>
2020-07-02 22:25:08 -0500

    urls finales

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from inicio.views import myHomeView, otherView	
-from . import views
+from django.conf import settings
+from django.conf.urls.static import static 
+from accounts.views import register
+from accounts.views import login
+from accounts.views import logout
+
 
 urlpatterns = [
-    path("register",views.register,name="register")
+	path('',include('travello.urls')),
+    path('admin/', admin.site.urls),
+    path('',myHomeView, name="pagina de inicio"),
+    path('otro/',otherView, name="otra pagina"),
+    #path('accounts/',include('accounts.urls')),
+    path('accounts/register',register,name="register"),
+    path('accounts/login',login,name="login"),
+    path('accounts/logout',logout,name="logout")
+    
 ]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)	
