165fcb92d8de545982569bc473385803817f279d
Algus <64106681+alvaro865@users.noreply.github.com>
2020-07-10 19:39:57 -0500

    creando las funciones para el formulario crear destinos

--- views.py
+++ views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render
 from .models import Destination
+#from .models import travello
+from .forms import travelloForm
 
 # Create your views here.
-def index(request, *args,**kwargs):
-	dest1 = Destination()
-	dest1.nombreCiudad='Munbai'
-	dest1.descripcionCiudad= 'The city that never sleeps'
-	dest1.imagenCiudad = 'destination_1.jpg'
-	dest1.precioTour = 701
-	dest1.ofertaTour = False
-
-	dest2 = Destination()
-	dest2.nombreCiudad='Hiderabad'
-	dest2.descripcionCiudad= 'The city '
-	dest2.imagenCiudad = 'destination_2.jpg'
-	dest2.precioTour = 700
-	dest2.ofertaTour = True
-
-	dest3 = Destination()
-	dest3.nombreCiudad='Bengalarub'
-	dest3.descripcionCiudad= 'Beatiful city '
-	dest3.imagenCiudad = 'destination_3.jpg'
-	dest3.precioTour = 702
-	dest3.ofertaTour = False
-
-	dests =[dest1,dest2,dest3]
+def index(request):
+	
+	dests = Destination.objects.all()
 	return render(request,"index.html", {'dests' : dests})
+
+def travelloTestView(request):
+	obj = travello.objects.get(id = 1)
+	context = {
+		'objecto' : obj,
+	}
+	return render(request,'descripcion.html',context)
+
+def destinoCreate(request):
+	form = travelloForm(request.POST or None)
+	context ={}
+	if form.is_valid():
+		form.save()
+		my_form = travelloForm()
+
+		context = {
+		'forms' : my_form
+	} 
+	return render(request,'destinoCreate.html',context)
