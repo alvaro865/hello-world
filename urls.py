67863aaafd88837fd9b4fc52b0627761fb8046b1
Algus <64106681+alvaro865@users.noreply.github.com>
2020-07-10 19:44:46 -0500

    agregando urls de crearDestino y accounts agregar

--- urls.py
+++ urls.py
@@ -17,7 +17,25 @@ from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView, otherView	
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static 
+from accounts.views import register
+from accounts.views import login
+from accounts.views import logout
+from travello.views import travelloTestView, destinoCreate
 
 urlpatterns = [
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
+    path('',include('travello.urls')),
+    path('admin/', admin.site.urls),
+    path('',myHomeView, name="pagina de inicio"),
+    path('otro/',otherView, name="otra pagina"),
+    #path('accounts/',include('accounts.urls')),
+    path('accounts/register',register,name="register"),
+    path('accounts/login',login,name="login"),
+    path('accounts/logout',logout,name="logout"),
+    path('crearDestino',travelloTestView,name="crear destino"),
+    path('accounts/agregar',destinoCreate,name="agregar destino"),
 ]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)	
